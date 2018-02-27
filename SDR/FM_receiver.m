2ece7a60f4a6367fbdbe153dfd31700c969542c4
sorru94 <36521570+sorru94@users.noreply.github.com>
2018-02-27 15:55:42 +0100

    Code reorganization

--- SDR/FM_receiver.m
+++ SDR/FM_receiver.m
@@ -1,35 +1,34 @@
-[z_out,z_B2,z_N2,z_dis,y_N1,y_B1] = FM_demod(x,B1,N1,B2,N2,fs)
+function [final,disc_out_filt,disc_out_filt_dec,disc_out,xfilt_dec,xfilt] = FM_receiver(x,B1,N1,B2,N2,fs)
+%Call with: [x,y,t,r,e,w] = FM_receiver('capture97R1k.bin',100E3,10,15E3,5,2.4E6);
+
 % To get a radio 10:
 % SDR>rtl_sdr -s 2400000 -f 103800000 -g 25 capture103R1k.bin
 
-x = loadFile('capture97R1k.bin');
+%If needed to print the PSD:
+%simpleSA(x,2^14,fc);
+
+x = loadFile(x);
 
 %initial sampling frequency
-fc = 2.4E6;
+fc = fs;
 %sampling frequency for the filters (Half-Band)
 fb = fc/2;
 %cut off frequency of the first filter
-fs = 100E3;
-%First decimation parameter
-N1 = 10;
-
-%simpleSA(x,2^14,fc);
+fs = B1;
 
 xfilt = LP_filter(x, fs/fb);
-xfilt_dec = Decim(xfilt,N1);
+xfilt_dec = downsample(xfilt,N1);
 
 %intermediate sampling frequency
 fc = fc/N1;
 %intermediate sampling frequency for the filters (Half-Band)
 fb = fc/2;
 %cut off frequency for second filter
-fs = 15E3;
+fs = B2;
 
 disc_out = discrim(xfilt_dec);
 disc_out_filt = LP_filter(disc_out, fs/fb);
-
-N2 = 5;
-disc_out_filt_dec = Decim(disc_out_filt,N2); 
+disc_out_filt_dec = downsample(disc_out_filt,N2);
 
 fc = fc/N2;
 
@@ -46,3 +45,4 @@ final = filter(b,a,disc_out_filt_dec);
 mx = max(abs(final));
 disc_out_filt_n = disc_out_filt_dec/mx;
 sound(disc_out_filt_n,fc);
+
--- SDR/LP_filter.m
+++ SDR/LP_filter.m
@@ -1,6 +1,4 @@
 function y = LP_filter(x, m)
 
-
 blo = fir1(63,m);
-
 y = filter(blo,1,x);
