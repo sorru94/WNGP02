7c24dcf4b00c3fc9cd651d85e4baa0b35a2f07b3
sorru94 <36521570+sorru94@users.noreply.github.com>
2018-04-04 21:47:18 +0200

    Added files

---
+++ Rate_Control_Assignment/Script.m
@@ -0,0 +1,273 @@
+function res = Script(a, M, CB, Pl, DelProf, Dist, num)
+
+M = M-1;
+cfgVHT = wlanVHTConfig;         
+cfgVHT.ChannelBandwidth = CB; % 40 MHz channel bandwidth
+cfgVHT.MCS = M;                    % QPSK rate-1/2
+cfgVHT.APEPLength = Pl;%4096,2048;          % APEP length in bytes
+
+% Set random stream for repeatability of results
+s = rng(21);
+
+%% Channel Configuration
+
+tgacChannel = wlanTGacChannel;
+tgacChannel.DelayProfile = DelProf;
+tgacChannel.ChannelBandwidth = cfgVHT.ChannelBandwidth;
+tgacChannel.NumTransmitAntennas = 1;
+tgacChannel.NumReceiveAntennas = 1;
+tgacChannel.TransmitReceiveDistance = Dist; % Distance in meters for NLOS
+tgacChannel.RandomStream = 'mt19937ar with seed';
+tgacChannel.Seed = 10;
+
+% Set the sampling rate for the channel
+sr = wlanSampleRate(cfgVHT);
+tgacChannel.SampleRate = sr;
+
+
+%% Simulation Parameters
+
+numPackets = num; % Number of packets transmitted during the simulation 
+walkSNR = false; 
+
+% Select SNR for the simulation
+if walkSNR
+    meanSNR = 22;   % Mean SNR
+    amplitude = 14; % Variation in SNR around the average mean SNR value
+    % Generate varying SNR values for each transmitted packet
+    baseSNR = sin(linspace(1,10,numPackets))*amplitude+meanSNR;
+    snrWalk = baseSNR(1); % Set the initial SNR value
+    % The maxJump controls the maximum SNR difference between one
+    % packet and the next 
+    maxJump = 0.5;
+else
+    % Fixed mean SNR value for each transmitted packet. All the variability
+    % in SNR comes from a time varying radio channel
+    snrWalk = 22; %#ok<UNRCH>
+end
+
+% To plot the equalized constellation for each spatial stream set
+% displayConstellation to true
+displayConstellation = false;
+if displayConstellation
+    ConstellationDiagram = comm.ConstellationDiagram; %#ok<UNRCH>
+    ConstellationDiagram.ShowGrid = true;
+    ConstellationDiagram.Name = 'Equalized data symbols';
+end
+
+% Define simulation variables
+snrMeasured = zeros(1,numPackets);
+MCS = zeros(1,numPackets);
+ber = zeros(1,numPackets);
+packetLength = zeros(1,numPackets);
+
+SNR_arr = zeros(1,numPackets);
+
+
+for numPkt = 1:numPackets 
+    if walkSNR
+        % Generate SNR value per packet using random walk algorithm biased
+        % towards the mean SNR
+        snrWalk = 0.9*snrWalk+0.1*baseSNR(numPkt)+rand(1)*maxJump*2-maxJump;
+    end
+    
+    % Generate a single packet waveform
+    txPSDU = randi([0,1],8*cfgVHT.PSDULength,1,'int8');
+    txWave = wlanWaveformGenerator(txPSDU,cfgVHT,'IdleTime',5e-4);
+    
+    % Receive processing, including SNR estimation
+    y = processPacket(txWave,snrWalk,tgacChannel,cfgVHT, a);
+    
+    % Plot equalized symbols of data carrying subcarriers
+    if displayConstellation && ~isempty(y.EstimatedSNR)
+        release(ConstellationDiagram);
+        ConstellationDiagram.ReferenceConstellation = helperReferenceSymbols(cfgVHT);
+        ConstellationDiagram.Title = ['Packet ' int2str(numPkt)];
+        ConstellationDiagram(y.EqDataSym(:));
+        drawnow 
+    end
+    
+    % Store estimated SNR value for each packet
+    if isempty(y.EstimatedSNR) 
+        snrMeasured(1,numPkt) = NaN;
+    else
+        snrMeasured(1,numPkt) = y.EstimatedSNR;
+    end
+    
+    % Determine the length of the packet in seconds including idle time
+    packetLength(numPkt) = y.RxWaveformLength/sr;
+    
+    % Calculate packet error rate (PER)
+    if isempty(y.RxPSDU)
+        % Set the PER of an undetected packet to NaN
+        ber(numPkt) = NaN;
+    else
+        [~,ber(numPkt)] = biterr(y.RxPSDU,txPSDU);
+    end
+
+    % used for the next packet
+    MCS(numPkt) = cfgVHT.MCS; % Store current MCS value
+end
+
+%% Display and Plot Simulation Results
+
+% Display and plot simulation results
+%disp(['Overall data rate: ' num2str(8*cfgVHT.APEPLength*(numPackets-numel(find(ber)))/sum(packetLength)/1e6) ' Mbps']);
+%disp(['Overall packet error rate: ' num2str(numel(find(ber))/numPackets)]);
+
+%plotResults(ber,packetLength,snrMeasured,MCS,cfgVHT);
+
+% Restore default stream
+rng(s);
+
+%Calculate channel efficiency
+if cfgVHT.ChannelBandwidth == 'CBW40'
+    B = 40000000;
+else
+    if cfgVHT.ChannelBandwidth == 'CBW20'
+        B = 20000000;
+    else
+        B = 80000000;
+    end
+end
+
+m = 1;
+eff = zeros(100,1);
+payload = 0;
+while m<=100
+    if ber(m) == 0
+        payload = cfgVHT.APEPLength;
+    end
+    eff(m) = (payload/(B*packetLength(m)*log2(1+snrMeasured(m))));
+    m = m+1;
+    payload = 0;
+end
+
+res = mean(eff);
+
+%displayEndOfDemoMessage(mfilename)
+
+end
+
+function Y = processPacket(txWave,snrWalk,tgacChannel,cfgVHT, a)
+    % Pass the transmitted waveform through the channel, perform
+    % receiver processing, and SNR estimation.
+    
+    chanBW = cfgVHT.ChannelBandwidth; % Channel bandwidth
+    % Set the following parameters to empty for an undetected packet
+    estimatedSNR = [];
+    eqDataSym = [];
+    noiseVarVHT = [];
+    rxPSDU = [];
+    
+    % Get the number of occupied subcarriers in VHT fields
+    [vhtData,vhtPilots] = helperSubcarrierIndices(cfgVHT,'VHT');
+    Nst_vht = numel(vhtData)+numel(vhtPilots);
+    Nfft = helperFFTLength(cfgVHT); % FFT length
+    
+    % Pass the waveform through the fading channel model
+    rxWave = tgacChannel(txWave);
+    
+    % Create an instance of the AWGN channel for each transmitted packet
+    awgnChannel = comm.AWGNChannel;
+    awgnChannel.NoiseMethod = 'Signal to noise ratio (SNR)';
+    % Normalization
+    awgnChannel.SignalPower = 1/tgacChannel.NumReceiveAntennas;
+    % Account for energy in nulls
+    %awgnChannel.SNR = a;
+    %awgnChannel.SNR = snrWalk-10*log10(Nfft/Nst_vht);
+    awgnChannel.SNR = a-10*log10(Nfft/Nst_vht);
+
+    % Add noise
+    rxWave = awgnChannel(rxWave);
+    rxWaveformLength = size(rxWave,1); % Length of the received waveform
+    
+    % Recover packet
+    ind = wlanFieldIndices(cfgVHT); % Get field indices
+    pktOffset = wlanPacketDetect(rxWave,chanBW); % Detect packet
+    
+    if ~isempty(pktOffset) % If packet detected
+        % Extract the L-LTF field for fine timing synchronization
+        LLTFSearchBuffer = rxWave(pktOffset+(ind.LSTF(1):ind.LSIG(2)),:);
+    
+        % Start index of L-LTF field
+        finePktOffset = wlanSymbolTimingEstimate(LLTFSearchBuffer,chanBW);
+     
+        % Determine final packet offset
+        pktOffset = pktOffset+finePktOffset;
+        
+        if pktOffset<15 % If synchronization successful
+            % Extract L-LTF samples from the waveform, demodulate and
+            % perform noise estimation
+            LLTF = rxWave(pktOffset+(ind.LLTF(1):ind.LLTF(2)),:);
+            demodLLTF = wlanLLTFDemodulate(LLTF,chanBW);
+
+            % Estimate noise power in non-HT fields
+            noiseVarVHT = helperNoiseEstimate(demodLLTF,chanBW,cfgVHT.NumSpaceTimeStreams,'Per Antenna');
+
+            % Extract VHT-LTF samples from the waveform, demodulate and
+            % perform channel estimation
+            VHTLTF = rxWave(pktOffset+(ind.VHTLTF(1):ind.VHTLTF(2)),:);
+            demodVHTLTF = wlanVHTLTFDemodulate(VHTLTF,cfgVHT);
+            chanEstVHTLTF = wlanVHTLTFChannelEstimate(demodVHTLTF,cfgVHT);
+
+            % Recover equalized symbols at data carrying subcarriers using
+            % channel estimates from VHT-LTF
+            [rxPSDU,~,eqDataSym] = wlanVHTDataRecover( ...
+                rxWave(pktOffset + (ind.VHTData(1):ind.VHTData(2)),:), ...
+                chanEstVHTLTF,mean(noiseVarVHT),cfgVHT);
+            
+            % SNR estimation per receive antenna
+            powVHTLTF = mean(VHTLTF.*conj(VHTLTF));
+            estSigPower = powVHTLTF-noiseVarVHT;
+            estimatedSNR = 10*log10(mean(estSigPower./noiseVarVHT));
+        end
+    end
+    
+    % Set output
+    Y = struct( ...
+        'RxPSDU',           rxPSDU, ...
+        'EqDataSym',        eqDataSym, ...
+        'RxWaveformLength', rxWaveformLength, ...
+        'NoiseVar',         noiseVarVHT, ...
+        'EstimatedSNR',     estimatedSNR);
+    
+end
+
+function plotResults(ber,packetLength,snrMeasured,MCS,cfgVHT)
+    % Visualize simulation results
+
+    figure('Outerposition',[50 50 900 700])
+    subplot(4,1,1);
+    plot(MCS);
+    xlabel('Packet Number')
+    ylabel('MCS')
+    title('MCS selected for transmission')
+
+    subplot(4,1,2);
+    plot(snrMeasured);
+    xlabel('Packet Number')
+    ylabel('SNR')
+    title('Estimated SNR')
+
+    subplot(4,1,3);
+    plot(find(ber==0),ber(ber==0),'x') 
+    hold on; stem(find(ber>0),ber(ber>0),'or') 
+    if any(ber)
+        legend('Successful decode','Unsuccessful decode') 
+    else
+        legend('Successful decode') 
+    end
+    xlabel('Packet Number')
+    ylabel('BER')
+    title('Instantaneous bit error rate per packet')
+
+    subplot(4,1,4);
+    windowLength = 3; % Length of the averaging window
+    movDataRate = movsum(8*cfgVHT.APEPLength.*(ber==0),windowLength)./movsum(packetLength,windowLength)/1e6;
+    plot(movDataRate)
+    xlabel('Packet Number')
+    ylabel('Mbps')
+    title(sprintf('Throughput over last %d packets',windowLength))
+    
+end
---
+++ Rate_Control_Assignment/Script3.m
@@ -0,0 +1,94 @@
+%% Measurement 1
+
+result1 = zeros(45,9);
+ChannelBandwidth = 'CBW40'; %CBW40, CBW20, CBW80, CBW160
+payload = 4096; %1024,4096,2048 NB 8192 is too big!!
+DelayProfile = 'Model-F';
+TransmitReceiveDistance = 20;
+numPackets = 100;
+l = [];
+%NB MCS tested is (M-1)
+for M = 1:10
+    for SNR = 6:45
+        result1(SNR, M) = Script(SNR, M, ChannelBandwidth, payload, DelayProfile, TransmitReceiveDistance,numPackets);
+       l(1) = SNR;
+        l(2) = M-1;
+    end
+end
+
+%% Measurement 2
+measurement = 2
+
+result2 = zeros(45,9);
+ChannelBandwidth = 'CBW40';
+payload = 4096; %4096,2048
+DelayProfile = 'Model-D';
+TransmitReceiveDistance = 10;
+
+l = [];
+%NB MCS tested is (M-1)
+for M = 1:10
+    for SNR = 6:45
+        result2(SNR, M) = Script(SNR, M, ChannelBandwidth, payload, DelayProfile, TransmitReceiveDistance,numPackets);
+       l(1) = SNR;
+        l(2) = M-1;
+    end
+end
+
+
+%% Measurement 3
+measurement = 3
+
+result3 = zeros(45,9);
+ChannelBandwidth = 'CBW40';
+payload = 4096; %4096,2048
+DelayProfile = 'Model-D';
+TransmitReceiveDistance = 30;
+
+l = [];
+%NB MCS tested is (M-1)
+for M = 1:10
+    for SNR = 6:45
+        result3(SNR, M) = Script(SNR, M, ChannelBandwidth, payload, DelayProfile, TransmitReceiveDistance,numPackets);
+       l(1) = SNR;
+        l(2) = M-1;
+    end
+end
+
+%% Measurement 4
+measurement = 4
+
+result4 = zeros(45,9);
+ChannelBandwidth = 'CBW40';
+payload = 4096; %4096,2048
+DelayProfile = 'Model-D';
+TransmitReceiveDistance = 50;
+
+l = [];
+%NB MCS tested is (M-1)
+for M = 1:10
+    for SNR = 6:45
+        result4(SNR, M) = Script(SNR, M, ChannelBandwidth, payload, DelayProfile, TransmitReceiveDistance,numPackets);
+       l(1) = SNR;
+        l(2) = M-1;
+    end
+end
+
+%% Measurement 5
+measurement = 5
+
+result5 = zeros(45,9);
+ChannelBandwidth = 'CBW40';
+payload = 4096; %4096,2048
+DelayProfile = 'Model-D';
+TransmitReceiveDistance = 5;
+
+l = [];
+%NB MCS tested is (M-1)
+for M = 1:10
+    for SNR = 6:45
+        result5(SNR, M) = Script(SNR, M, ChannelBandwidth, payload, DelayProfile, TransmitReceiveDistance,numPackets);
+       l(1) = SNR;
+        l(2) = M-1;
+    end
+end
